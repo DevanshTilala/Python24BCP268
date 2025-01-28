92f35b4d586c04601b89d5b8eb5bfbf28f8ee501
DevanshTilala <tilaladevansh@gmail.com>
2025-01-28 12:25:00 +0530

    Delete Second.py

--- Second.py
+++
@@ -1,8 +0,0 @@
-hour=float(input("Enter time in hours: "))
-minute=hour*60
-print("Time in minutes is",minute)
-
-minute=float(input("Enter time in minutes: "))
-hour=minute/60
-print("Time in hours is",hour)
-
