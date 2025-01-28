0e6b898181c2a4e5ca341cce77f797c8ab4a539f
DevanshTilala <tilaladevansh@gmail.com>
2025-01-28 12:25:27 +0530

    Delete Third.py

--- Third.py
+++
@@ -1,7 +0,0 @@
-dollar=float(input("Enter amount in dollar: "))
-rupee=dollar*85
-print("Amount in rupees is",rupee)
-
-rupee=float(input("Enter amount in rupee: "))
-dollar=rupee/85
-print("Amount in dollar is",dollar)
