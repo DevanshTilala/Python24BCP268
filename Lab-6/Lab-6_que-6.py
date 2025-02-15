f6e57e16bea5ef691f15bdf98a19cf07572791d9
DevanshTilala <tilaladevansh@gmail.com>
2025-02-15 21:50:55 +0530

    Add files via upload

--- Lab-6/Lab-6_que-6.py
+++ Lab-6/Lab-6_que-6.py
@@ -4,13 +4,23 @@ def modifying_tuple(tup, index, num):
     return tuple(tup)
 
 tup=(1,2,3,4,5,6,7,8,9)
+print(f"Tuple is {tup}")
 # print(tup)
 # tup=list(tup)
 # n1=int(input("Enter the number you want to replace: "))
-i=int(input("Enter the index at which you want to replace number: "))
-n=int(input("Enter the number you want to replace with: "))
+try:
+    i=int(input("Enter the index at which you want to replace number: "))
+    n=int(input("Enter the number you want to replace with: "))
+except ValueError:
+    print("\n\t\t!!! Please enter the valid number !!!")
+    
 
-print(modifying_tuple(tup, i, n))
+try:
+    print(f"Modified tuple is {modifying_tuple(tup, i, n)}")
+except IndexError:
+    print("\n\t\t!!! Index out of range. Please enter the valid value for index !!!")
+except ValueError and NameError:
+    print("\n\t\t!!! Please enter the valid number !!!")
 # for i in range(0,len(tup)):
 #     if tup[i]==n1:
 #         tup[i]=n2
--- Lab-6/Lab-6_que-7.py
+++ Lab-6/Lab-6_que-7.py
@@ -2,6 +2,8 @@ def del_element_tuple(tup, num):
     tup=list(tup)
     if num in tup:
         tup.remove(num)
+    else:
+        return 0
 
     return tuple(tup)
 
@@ -9,4 +11,7 @@ tup=(1,2,3,4,5,6,7,8,9)
 print(tup)
 n=int(input("Enter the number you want to remove: "))
 
-print(del_element_tuple(tup,n))
+if(del_element_tuple(tup,n)):
+    print(f"Modified tuple is {del_element_tuple(tup,n)}")
+else:
+    print("Element not in tuple")
