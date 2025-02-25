1c6afa710fbbc4319bd53eb40b5838caef21db63
DevanshTilala <tilaladevansh@gmail.com>
2025-02-25 11:17:33 +0530

    Add files via upload

--- Lab-8/Lab-8_que-1.py
+++ Lab-8/Lab-8_que-1.py
@@ -2,17 +2,7 @@ def word_to_upper(string):
     string_set=set()
 
     for i in string:
-        temp_string=""
-        for j in i:
-            # if 'A' <= j <= 'Z':
-            #     j=chr(ord(j)+32)
-            #     temp_string+=j
-            if 'a' <= j <= 'z':
-                j=chr(ord(j)-32)
-                temp_string+=j
-            else:
-                temp_string+=j
-        string_set.add(temp_string)
+        string_set.add(i.upper())
 
     return string_set
 
