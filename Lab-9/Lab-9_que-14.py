844d2544f55902ae408c74d366017ed4430818bd
DevanshTilala <tilaladevansh@gmail.com>
2025-03-18 12:43:53 +0530

    Add files via upload

--- Lab-9/Lab-9_que-14.py
+++ Lab-9/Lab-9_que-14.py
@@ -1,8 +1,11 @@
 def count_vowels(string1, count=0, i=0):
     vowels="aeiouAEIOU"
-    if string1[i] in vowels:
-        return count + count_vowels(string1,count,i+1)
+    if i==len(string1):
+        return count
+    elif string1[i] in vowels:
+        return count + 1 + count_vowels(string1,count,i+1)
     else:
         return count_vowels(string1,count,i+1)
 
-print(count_vowels("A man a dream a reality"))
+str1=input("Enter a string: ")
+print(f"Number of vowels in the string are: {count_vowels(str1)}")
