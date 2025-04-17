4cf1190aaeb2b5604986a3c77780b2a02d8b95f6
unknown <tilaladevansh@gmail.com>
2025-04-17 15:55:21 +0530

    Lab-10

--- Lab-10_que-4.py
+++
@@ -1,9 +0,0 @@
-import shutil,os
-
-if os.path.exists("C:\\Users\\lab\\Desktop\\24BCP268\\practice"):
-    print("Path already exists")
-else:
-    os.mkdir('C:\\Users\\lab\\Desktop\\24BCP268\\practice')
-    print("Made new directory")
-
-shutil.copyfile('C:\\Users\\lab\\Desktop\\24BCP309 PYTHON SERIES\\ADD.py','C:\\Users\\lab\\Desktop\\24BCP268\\practice\\ADD.py')
